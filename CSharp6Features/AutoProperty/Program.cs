2a4b6024cf414b5b6a40c263e9a4e28498c4923a
Sander Gerz <sander@devtips.net>
2014-11-28 16:53:00 +0000

    Exception Filters

--- CSharp6Features/AutoProperty/Program.cs
+++ CSharp6Features/AutoProperty/Program.cs
@@ -6,6 +6,53 @@ using System.Threading.Tasks;
 
 namespace AutoProperty
 {
+    // 1. Initializers for auto-properties 
+    public class ReceptOud
+    {
+        public string Titel { get; set; }
+        public int Niveau { get; set; }
+        public ReceptOud()
+        {
+            this.Titel = "[Nieuw]";
+            this.Niveau = 300;
+        }
+    }
+
+    public class ReceptNieuw
+    {
+        public string Titel { get; set; } = "[Nieuw]";
+        public int Niveau { get; set; } = 300;
+    }
+
+
+    // 2. Getter-only auto-properties 
+    public class IngredientOud
+    {
+        public int Aantal { get; private set; }
+        public IngredientOud()
+        {
+            this.Aantal = 5;
+        }
+    }
+
+    public class IngredientNieuw
+    {
+        public int Aantal { get; } = 5;
+    }
+
+
+    // 3. Ctor assignment to getter-only autoprops
+    public class Keuken
+    {
+        public string Naam { get; }
+        public Keuken()
+        {
+            // Dit kon niet in =< C# 5.0
+            this.Naam = "Frans";
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
---
+++ CSharp6Features/Structs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+
+    // 1. Parameterless constructors in structs    
+    struct Duur
+    {
+        public int Minuten;
+        public int Uur;
+        public Duur(int uur, int minuten)
+        {
+            Uur = uur;
+            Minuten = minuten;
+        }
+
+        // Dit kon niet in =< C# 5.0
+        public Duur() : this(1, 30) { }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
